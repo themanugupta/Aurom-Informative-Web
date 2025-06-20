9b0d71b5737b510e6090d1b4f26a427b608d1ffd
Rahul Malik <arahulmalik25@gmail.com>
2025-06-20 18:34:00 +0530

    fixed vite file

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    allowedHosts: [
-      "7fd8-2409-40d6-10c4-29b4-dd06-c300-d10a-de8d.ngrok-free.app",
-    ],
-  },
+  
 });
